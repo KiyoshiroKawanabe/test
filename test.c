a889eee1dc036b085458f182665abd203d9a9a97
Kiyo <s19C1031UL@s.chibakoudai.jp>
2020-10-15 13:40:12 +0900

    Add a file

---
+++ test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int x,y;
+  printf("最初の数字頂戴");
+  scanf("%d",&x);
+  printf("２つめの数字頂戴");
+  scanf("%d",&y);
+  printf("じゃあ足すね。");
+  printf("%d\n", x+y);
+
+  return 0;
+  }
